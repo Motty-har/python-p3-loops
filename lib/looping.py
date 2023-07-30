5107dfda239b67e9ea6210032b45520e5a39a886
Motty-har <harligmotty@gmail.com>
2023-07-29 23:21:54 -0400

    Finished

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i in range(10, 0, -1):
+        print(i)
+        i -= 1 
+    
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [int **2 for int in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
+    i = 0
+    while i in range(0, 100, 1):
+        i += 1
+        if i%3 == 0 and i%5 == 0:
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
